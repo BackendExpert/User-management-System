593dbef0dac11a80a4559af50afe16f737584e4d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-29 14:06:29 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/server.js
+++ server/server.js
@@ -9,7 +9,11 @@ const salt = 10;
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:5173"],
+    methods: ["POST", "GET"],
+    credentials: true
+}));
 app.use(cookieParser());
 
 

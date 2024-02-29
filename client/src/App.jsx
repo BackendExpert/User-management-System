7385d6efa363c53e3865f8a04008c4a85d5e0f17
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-29 11:11:56 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SignIn from "./components/LoginSignUp/SignIn";
 import SignUp from "./components/LoginSignUp/SignUp";
+import Admin from "./components/Admin/Admin";
+import Users from "./components/Users/Users";
 
 
 export default function App() {
@@ -9,6 +11,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<SignIn />} ></Route>
         <Route path="/register" element={<SignUp />} ></Route>
+        <Route path="/admin" element={<Admin />} ></Route>
+        <Route path="/user" element={<Users />} ></Route>
       </Routes>
     </BrowserRouter>
   )

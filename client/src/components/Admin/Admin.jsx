1ca7c5ebf552b429b0e779b190711e4678d52d2c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-29 14:24:29 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Admin/Admin.jsx
+++ client/src/components/Admin/Admin.jsx
@@ -1,6 +1,26 @@
-import { Redirect } from "react-router-dom"
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { Redirect, useNavigate } from "react-router-dom"
 
 const Admin = () => {
+    const [auth, SetAuth] = useState(false);
+    const [message, SetMessage] = useState('');
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        axios.get('/')
+        .then(res => {
+            if(res.data.Status === "Success"){
+                SetAuth(true)
+                navigate('/')
+            }
+            else{
+                SetAuth(false)
+                SetMessage(res.data.Error)
+            }
+        })
+        .then(err => console.log(err))
+    }, [])
   return (
     <div className="px-16 py-12">
         {

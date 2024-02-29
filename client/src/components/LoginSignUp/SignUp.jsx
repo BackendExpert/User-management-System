86efa0432fd402165b82fe77c3c95ee431d3a682
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-29 11:18:32 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/LoginSignUp/SignUp.jsx
+++ client/src/components/LoginSignUp/SignUp.jsx
@@ -18,19 +18,23 @@ const SignUp = () => {
                 <form>
                     <div className="">
                         <label htmlFor="" className="text-xl py-4">Username : </label>
-                        <input type="text" className="pl-4 border w-full h-12 my-2 rounded text-xl" required placeholder="Username" />
+                        <input type="text" className="pl-4 border w-full h-12 my-2 rounded text-xl" required placeholder="Username" 
+                        onChange={e => SetValues({...values, username: e.target.value})}/>
                     </div>
                     <div className="">
                         <label htmlFor="" className="text-xl py-4">First Name : </label>
-                        <input type="text" className="pl-4 border w-full h-12 my-2 rounded text-xl" required placeholder="First Name" />
+                        <input type="text" className="pl-4 border w-full h-12 my-2 rounded text-xl" required placeholder="First Name" 
+                        onChange={e => SetValues({...values, fname: e.target.value})}/>
                     </div>
                     <div className="">
                         <label htmlFor="" className="text-xl py-4">Email : </label>
-                        <input type="email" className="pl-4 border w-full h-12 my-2 rounded text-xl" required placeholder="Email" />
+                        <input type="email" className="pl-4 border w-full h-12 my-2 rounded text-xl" required placeholder="Email" 
+                        onChange={e => SetValues({...values, email: e.target.value})}/>
                     </div>
                     <div className="">
                         <label htmlFor="" className="text-xl py-4">Password : </label>
-                        <input type="password" className="pl-4 border w-full h-12 my-2 rounded text-xl" required placeholder="Password" />
+                        <input type="password" className="pl-4 border w-full h-12 my-2 rounded text-xl" required placeholder="Password" 
+                        onChange={e => SetValues({...values, password: e.target.value})}/>
                     </div>
                     <div className="">
                         <button type="submit" className="h-12 w-1/2 rounded-md border border-cyan-500 text-cyan-500 duration-500 hover:bg-cyan-500 hover:text-white">SignUp</button>

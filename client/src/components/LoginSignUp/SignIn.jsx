ceea34ab429d93ae3634cf02a5371dc9ccf9416a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-29 09:28:08 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/LoginSignUp/SignIn.jsx
+++ client/src/components/LoginSignUp/SignIn.jsx
@@ -3,19 +3,19 @@ import { Link } from "react-router-dom"
 const SignIn = () => {
   return (
     <div className="bg-blue-500 w-full h-screen">
-        <div className="container px-32 py-20 ">
-            <div className="rounded-xl mx-40 p-20 mt-20 bg-white w-full">
+        <div className="container lg:px-32 py-10 ">
+            <div className="rounded-xl lg:mx-20 p-10 bg-white w-full">
                 <p className="text-3xl font-semibold py-4">SignIn</p>
                 <hr className="pb-6" />
 
                 <form>
                     <div className="">
                         <label htmlFor="" className="text-xl py-4">Email : </label>
-                        <input type="email" className="pl-4 border w-full h-16 my-2 rounded text-2xl" required placeholder="Email" />
+                        <input type="email" className="pl-4 border w-full h-12 my-2 rounded text-xl" required placeholder="Email" />
                     </div>
                     <div className="">
                         <label htmlFor="" className="text-xl py-4">Password : </label>
-                        <input type="password" className="pl-4 border w-full h-16 my-2 rounded text-2xl" required placeholder="Password" />
+                        <input type="password" className="pl-4 border w-full h-12 my-2 rounded text-xl" required placeholder="Password" />
                     </div>
                     <div className="">
                         <button type="submit" className="w-1/2 rounded-md border border-cyan-500 py-4 my-4 text-xl text-cyan-600 duration-500 hover:bg-cyan-500 hover:text-white">Login</button>
--- client/src/components/LoginSignUp/SignUp.jsx
+++ client/src/components/LoginSignUp/SignUp.jsx
@@ -2,28 +2,28 @@ import { Link } from "react-router-dom"
 
 const SignUp = () => {
   return (
-    <div className="bg-blue-500 w-full h-screen">
-        <div className="container px-32 py-20 ">
-            <div className="rounded-xl mx-40 p-20 mt-16 bg-white w-full">
+    <div className="bg-blue-500 w-full h-full">
+        <div className="container lg:px-32 py-10 ">
+            <div className="rounded-xl lg:mx-20 p-10 bg-white w-full">
                 <p className="text-3xl font-semibold py-4">SignUp</p>
                 <hr className="pb-6" />
 
                 <form>
                     <div className="">
                         <label htmlFor="" className="text-xl py-4">Username : </label>
-                        <input type="text" className="pl-4 border w-full h-16 my-2 rounded text-2xl" required placeholder="Username" />
+                        <input type="text" className="pl-4 border w-full h-12 my-2 rounded text-xl" required placeholder="Username" />
                     </div>
                     <div className="">
                         <label htmlFor="" className="text-xl py-4">First Name : </label>
-                        <input type="text" className="pl-4 border w-full h-16 my-2 rounded text-2xl" required placeholder="First Name" />
+                        <input type="text" className="pl-4 border w-full h-12 my-2 rounded text-xl" required placeholder="First Name" />
                     </div>
                     <div className="">
                         <label htmlFor="" className="text-xl py-4">Email : </label>
-                        <input type="email" className="pl-4 border w-full h-16 my-2 rounded text-2xl" required placeholder="Email" />
+                        <input type="email" className="pl-4 border w-full h-12 my-2 rounded text-xl" required placeholder="Email" />
                     </div>
                     <div className="">
                         <label htmlFor="" className="text-xl py-4">Password : </label>
-                        <input type="password" className="pl-4 border w-full h-16 my-2 rounded text-2xl" required placeholder="Password" />
+                        <input type="password" className="pl-4 border w-full h-12 my-2 rounded text-xl" required placeholder="Password" />
                     </div>
                     <div className="">
                         <button type="submit" className="h-12 w-1/2 rounded-md border border-cyan-500 text-cyan-500 duration-500 hover:bg-cyan-500 hover:text-white">SignUp</button>

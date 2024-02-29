1f18489c0397b6ccebbbb2570699606fa32b7cba
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-29 14:16:12 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Admin/Admin.jsx
+++ client/src/components/Admin/Admin.jsx
@@ -1,6 +1,16 @@
 const Admin = () => {
   return (
-    <div>Admin</div>
+    <div className="px-16 py-12">
+        {
+            auth ? 
+            <div className="">
+                Welcome Admin {}
+                <button className="px-8 py-4 border border-red-500 text-red-500"></button>
+            </div>
+            :
+            <div className=""></div>
+        }
+    </div>
   )
 }
 

4a86264796563cd10cbebf9e33e5bd84b29eb84e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-01 06:30:20 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/Admin/AdminDash.jsx
+++ client/src/components/Admin/AdminDash.jsx
@@ -1,8 +1,15 @@
+import LinksAdmin from "./AdminLinks"
 
 const AdminDash = () => {
   return (
     <div>
-
+        {
+          LinksAdmin.map((Link) => (
+            <div className="">
+              <div className="">{Link.name}</div>
+            </div>
+          ))
+        }
     </div>
   )
 }
